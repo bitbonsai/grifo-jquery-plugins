36cd53aad83cb87b8f0e186983f4f0dac73dc235
Mauricio Wolff <mauriciowolff@gmail.com>
2014-04-30 15:12:29 +0200

    Proper commas and semi-colons :)
    #KDNN

--- source/jquery.placeholder.js
+++ source/jquery.placeholder.js
@@ -10,61 +10,65 @@
  */
 ;(function($){
 $.fn.placeholder = function(opts){
-	
-	var options = $.extend({
-	    attr       : 'placeholder'
-	  , emptyClass : 'empty'
-	}, opts)
-	
-	// test for native support
-	if (options.attr === 'placeholder' && 'placeholder' in document.createElement('input')) return this
+    
+    var options = $.extend({
+        attr       : 'placeholder',
+        emptyClass : 'empty'
+    }, opts)
+    
+    // test for native support
+    if (options.attr === 'placeholder' && 'placeholder' in document.createElement('input')) {
+        return this;
+    }
 
-	return this.each(function(){
+    return this.each(function(){
 
-		var input = $(this)
-			, text = input.attr(options.attr)
-			, isPassword = input.attr('type') === 'password'
-			, replaced
+        var input = $(this),
+            text = input.attr(options.attr),
+            isPassword = input.attr('type') === 'password',
+            replaced
 
-		if (!text) return
+        if (!text){
+            return;
+        }
 
-		// replace password input with type=text to show placeholder text
-		// reverts back to type=password when focused
-		if (isPassword) {
-			replaced = $('<input type="text" />')
-				.attr({ value: text, className: input[0].className })
-				.insertAfter( input.hide() )
-		}
+        // replace password input with type=text to show placeholder text
+        // reverts back to type=password when focused
+        if (isPassword) {
+            replaced = $('<input type="text" />')
+                .attr({ value: text, className: input[0].className })
+                .insertAfter( input.hide() );
+        }
 
-		input.closest('form').submit(function(){
-			input.trigger('focus')
-		})
+        input.closest('form').submit(function(){
+            input.trigger('focus');
+        })
 
-		input.val(text).addClass(options.emptyClass)
+        input.val(text).addClass(options.emptyClass);
 
-		input.focus(function(){
-			if (input.val() == text)
-				input.val("").removeClass(options.emptyClass)
-		})
+        input.focus(function(){
+            if (input.val() == text) {
+                input.val("").removeClass(options.emptyClass);
+            }
+        });
 
-		input.blur(function(){
-			if ($.trim(this.value) != ""){
-				return
-			}
-			if (isPassword){
-				input.after(replaced).hide()
-			} else {
-				input.val(text).addClass(options.emptyClass)
-			}
-		})
+        input.blur(function(){
+            if ($.trim(this.value) != ""){
+                return;
+            }
+            if (isPassword){
+                input.after(replaced).hide();
+            } else {
+                input.val(text).addClass(options.emptyClass);
+            }
+        });
 
-		if (isPassword){
-		 	replaced.addClass(options.emptyClass).focus(function(){
-				replaced.detach()
-				input.show().focus()
-			})
-		}
-
-	})
+        if (isPassword){
+            replaced.addClass(options.emptyClass).focus(function(){
+                replaced.detach();
+                input.show().focus();
+            })
+        }
+    });
 }
 })(jQuery);
